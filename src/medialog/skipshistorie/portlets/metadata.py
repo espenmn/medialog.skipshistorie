6e2f70cba3364133ec402ed59c2a37661b2bfad1
Espen Moe-Nilssen <espen@medialog.no>
2023-03-23 13:05:10 +0100

    Portlet update

--- src/medialog/skipshistorie/portlets/metadata.py
+++ src/medialog/skipshistorie/portlets/metadata.py
@@ -17,47 +17,43 @@ import six.moves.urllib.request, six.moves.urllib.error, six.moves.urllib.parse
 
 
 class IMetadataPortlet(IPortletDataProvider):
-    place_str = schema.TextLine(
-        title=_(u'Name of your place with country code'),
-        description=_(u'City name along with country code i.e Delhi,IN'),  # NOQA: E501
-        required=True,
-        default=u'delhi,in'
-    )
+    title = schema.TextLine(
+       title=_(u'Portlet title'),
+       required=True,
+       default=u'Metadata'
+     )
 
 
 @implementer(IMetadataPortlet)
 class Assignment(base.Assignment):
     schema = IMetadataPortlet
 
-    def __init__(self, place_str='delhi,in'):
-        self.place_str = place_str.lower()
-
     @property
     def title(self):
-        return _(u'Weather of the place')
+        return _(u'Metadata')
 
 
 class AddForm(base.AddForm):
     schema = IMetadataPortlet
     form_fields = field.Fields(IMetadataPortlet)
-    label = _(u'Add Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Metadata portlet')
+    description = _(u'This portlet displays metadata info')
 
-    def create(self, data):
+    def create(self, something):
         return Assignment(
-            place_str=data.get('place_str', 'delhi,in'),
-        )
+             enabled = True
+    )
 
 
 class EditForm(base.EditForm):
     schema = IMetadataPortlet
     form_fields = field.Fields(IMetadataPortlet)
-    label = _(u'Edit Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Change title')
+    description = _(u'This portlet displays metadata info')
 
 
 class Renderer(base.Renderer):
-    schema = IMetadataPortlet
+    #schema = IMetadataPortlet
     _template = ViewPageTemplateFile('metadata.pt')
 
     def __init__(self, *args):
@@ -76,31 +72,50 @@ class Renderer(base.Renderer):
     def available(self):
         """Show the portlet only if there are one or more elements and
         not an anonymous user."""
-        return not self.anonymous and self._data()
+        return True
+        #return not self.anonymous and self._data()
 
-    def weather_report(self):
-        self.result = self._data()
-        return self.result['description']
+    def get_idold(self):
+        return self.context.idold
 
-    def get_humidity(self):
-        return self.result['humidity']
+    def get_idnew(self):
+        return self.context.idnew
 
-    def get_pressure(self):
-        return self.result['pressure']
+    def get_vessel(self):
+        return self.context.vessel
 
-    @memoize
-    def _data(self):
-        baseurl = 'https://query.yahooapis.com/v1/public/yql?'
-        yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="{0}")'.format(  # NOQA: E501
-            self.data.place_str,
-        )
-        yql_url = baseurl + six.moves.urllib.parse.urlencode(
-            {'q': yql_query},
-        ) + '&format=json'
-        result = six.moves.urllib.request.urlopen(yql_url).read()
-        data = json.loads(result)
-        result = {}
-        result['description'] = data['query']['results']['channel']['description']  # NOQA: E501
-        result['pressure'] = data['query']['results']['channel']['atmosphere']['pressure']  # NOQA: E501
-        result['humidity'] = data['query']['results']['channel']['atmosphere']['humidity']  # NOQA: E501
-        return result
+    def get_skipstype(self):
+        return self.context.skipstype
+
+    def get_grt(self):
+        return self.context.grt
+
+    def get_tdw(self):
+        return self.context.tdw
+
+    def get_purch(self):
+        return self.context.purch
+
+    def get_del(self):
+        return self.context.del_
+
+    def get_shipyard(self):
+        return self.context.shipyard
+
+    def get_ex(self):
+        return self.context.ex
+
+    def get_yno(self):
+        return self.context.yno
+
+    def get_owneri(self):
+        return self.context.owneri
+
+    def get_senerenorsk(self):
+        return self.context.senerenorsk
+
+    def get_sold(self):
+        return self.context.sold
+
+    def get_to(self):
+        return self.context.to
