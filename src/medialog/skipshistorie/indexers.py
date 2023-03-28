68574930dd9230b2cb8d4f793544f14cc48649bd
Espen Moe-Nilssen <espen@medialog.no>
2023-03-28 12:34:53 +0200

    update metadata portlet

---
+++ src/medialog/skipshistorie/indexers.py
@@ -0,0 +1,16 @@
+#UTF-8
+from plone.indexer import indexer
+from plone.app.contenttypes.interfaces import IDocument
+#from plone.indexer.decorator import indexer
+#import six
+#from plone.dexterity.utils import safe_unicode
+#from plone.dexterity.utils import safe_utf8
+
+@indexer(IDocument)
+def idnewIndexer(self):
+    return str(self.idnew)
+
+
+@indexer(IDocument)
+def off_nrIndexer(self):
+    return str(self.off_nr)
